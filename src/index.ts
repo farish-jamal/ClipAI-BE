d01c0559f265751d7cdd194d0ddbdcfa704d863f
farish-jamal <farishjamal98@gmail.com>
2025-10-26 03:04:03 +0530

    feat: update Prisma configuration and schema, add database connection handling

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,28 @@
-import 'dotenv/config';
-import http from 'http';
-import app from './app';
+import "dotenv/config";
+import http from "http";
+import app from "./app";
+import prisma from "./prisma/client";
 
 const PORT = process.env.PORT || 8080;
-const MONGO_URI = process.env.MONGO_URI;
+const DATABASE_URL = process.env.DATABASE_URL;
 
-if (!PORT || !MONGO_URI) {
-    console.error(`Missing required environment variables:`, { PORT, MONGO_URI });
-    process.exit(1);
+if (!PORT || !DATABASE_URL) {
+   console.error(`Missing required environment variables:`, { PORT, DATABASE_URL });
+   process.exit(1);
 }
+
+(async () => {
+   try {
+      await prisma.$connect();
+      console.log("Connected to the database successfully.");
+   } catch (error) {
+      console.error("Failed to connect to the database:", error);
+      process.exit(1);
+   }
+})();
 
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+   console.log(`Server started on port ${PORT}`);
 });
